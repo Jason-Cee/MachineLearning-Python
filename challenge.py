c57e39f88cfaaba9f82ee2d465619d7cb85fe86a
Jason-Cee <jasoncee017@gmail.com>
2021-06-01 13:33:56 +0200

    first commit

---
+++ challenge.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy import *
+
+a = np.arange(21)
+
+print("Challenge 01/06/2021")
+print(a)
+
+mean = np.mean(a)
+print("\nMean: ", mean)
+
+std = np.std(a)
+print("\nStandard Deviation: ", std)
+
+var = np.var(a)
+print("\nVariance: ", var)
